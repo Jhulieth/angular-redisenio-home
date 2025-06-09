e0c258a79fba4d325dda42746281df628f18a8bd
Daniela Osorio Sanchez <daniela.osorio@bayteq.com>
2025-06-09 18:02:48 -0500

    se habilita serviceworker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,13 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //    enabled: environment.enableServiceWorker,
-    //   //enabled: !isDevMode(),
-    //   // Register the ServiceWorker as soon as the application is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+       enabled: environment.enableServiceWorker,
+      //enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
